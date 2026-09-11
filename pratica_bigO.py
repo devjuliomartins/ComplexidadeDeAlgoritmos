b0602c67391924f354198c10487933638806938f
devjuliomartins <contatoeujuliomartins@gmail.com>
2026-09-11 13:55:29 -0300

    first commit

---
+++ pratica_bigO.py
@@ -0,0 +1,8 @@
+# Exemplo prático do Big O:O(1)
+
+numeros = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+# Acesso direto ao elemento de índice 5
+elemento = numeros[4]
+
+print("Elemento encontrado:", elemento)
